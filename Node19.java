a0c735e97af42e320dfeef9c94cb21cabb431686
radadevitasari <radadevitasari87.com>
2026-05-12 11:05:25 +0700

    Percobaan 1 dan modifikasi

---
+++ Node19.java
@@ -0,0 +1,12 @@
+public class Node19 {
+    Mahasiswa19 data;
+    Node19 prev;
+    Node19 next;
+
+    public Node19(Mahasiswa19 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+    
